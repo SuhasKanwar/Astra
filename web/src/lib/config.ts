8cae51db0bcc2f3bf584155ab3c8510360668eef
SuhasKanwar <suhas.kanwar@gmail.com>
2026-07-20 02:04:23 +0530

    Fixing the env varible

--- web/src/lib/config.ts
+++ web/src/lib/config.ts
@@ -1,4 +1,4 @@
-export const HTTP_SERVER_BASE_URL: string = process.env.HTTP_SERVER_BASE_URL || 'http://localhost:9000';
+export const HTTP_SERVER_BASE_URL: string = process.env.NEXT_PUBLIC_HTTP_SERVER_BASE_URL || 'http://localhost:9000';
 export const GOOGLE_CLIENT_ID: string = process.env.GOOGLE_CLIENT_ID!;
 export const GOOGLE_CLIENT_SECRET: string = process.env.GOOGLE_CLIENT_SECRET!;
 export const NEXTAUTH_SECRET: string = process.env.NEXTAUTH_SECRET || "secret";
